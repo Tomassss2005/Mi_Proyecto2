1ed39ff64805c6ca3fddec1b4a3477b14148ef4c
Tomassss2005 <tomidlp2005@gmail.com>
2024-11-22 12:32:01 -0300

    Primeros cambios

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+// src/app/layout.js
+'use client'; // Marca este archivo como componente cliente
+
+import { useEffect, useState } from 'react';
+import './globals.css';
+
+
+
+export default function RootLayout({ children }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Esto se ejecuta solo en el cliente
+  }, []);
+
+  if (!isClient) {
+    return <div>Loading...</div>; // Muestra un cargando mientras se monta el cliente
+  }
+
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
